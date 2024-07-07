9affa1d02a17cc781919f3c9ff03339845bf7f29
schmyga <93492278+schmyga@users.noreply.github.com>
2024-07-07 16:16:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function redirectToEmail() {
   var email = 'viktor@outlook.de';
   var cc = 'Rita@outlook.de';
   var subject = 'Beshtelung';
-  var body = 'Hallo zusammen,%0A%0A%0A - Хочу заказать товар %0A%0A%0ADanke !';
+  var body = 'Hallo zusammen,
+    
+    Хочу заказать товар
+      
+  
+  Danke !';
 
   var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) +
     '&cc=' + encodeURIComponent(cc) + '&body=' + encodeURIComponent(body);
