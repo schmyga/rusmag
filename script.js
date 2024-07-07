439d38f811433316cae0d39f99b2727b69923e38
schmyga <93492278+schmyga@users.noreply.github.com>
2024-07-07 16:27:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ function redirectToEmail() {
 }
 
 document.querySelector('.order-button').addEventListener('click', redirectToEmail);
+var expandBtn = document.querySelector('.expand-btn');
+var expandText = document.querySelector('.expand-text');
+
+expandBtn.addEventListener('click', function() {
+  if (expandText.style.display === 'none') {
+    expandText.style.display = 'inline';
+    expandBtn.textContent = 'Weniger anzeigen';
+  } else {
+    expandText.style.display = 'none';
+    expandBtn.textContent = 'Mehr anzeigen';
+  }
+});
+
