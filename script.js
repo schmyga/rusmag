18b50a029eccc8821a4d7250508848f504801935
schmyga <93492278+schmyga@users.noreply.github.com>
2024-07-07 12:57:19 +0200

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.querySelector('.order-button').addEventListener('click', function(mailto:viktor@outlook.de?subject=Beshtelung&cc=Rita@outlook.de&body=Hallo%20zusammen,%0A%0A%0A%20-%20Хочу ваши помидоры%0A%0A%0ADanke%20!) {
-  
-  // Действия при нажатии на кнопку "Заказать"
-  console.log('Заказ оформлен');
-});
+function redirectToEmail() {
+  var email = 'viktor@outlook.de';
+  var cc = 'Rita@outlook.de';
+  var subject = 'Beshtelung';
+  var body = 'Hallo zusammen,%0A%0A%0A - Хочу заказать товар %0A%0A%0ADanke !';
+
+  var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) +
+    '&cc=' + encodeURIComponent(cc) + '&body=' + encodeURIComponent(body);
+
+  window.location.href = mailtoLink;
+}
