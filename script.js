5294c3a49c91f6a448c1ac00da6ab3775d9c3b87
schmyga <93492278+schmyga@users.noreply.github.com>
2024-07-07 17:08:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ function redirectToEmail() {
   var email = 'viktor@outlook.de';
   var cc = 'Rita@outlook.de';
   var subject = 'Beshtelung';
-  var body = 'Hallo zusammen,
-    
-    Хочу заказать товар
-      
-  
-  Danke !';
+  var body = 'Hallo zusammen,%0A%0A%0A - Хочу заказать товар %0A%0A%0ADanke !';
 
   var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) +
     '&cc=' + encodeURIComponent(cc) + '&body=' + encodeURIComponent(body);
@@ -16,16 +11,3 @@ function redirectToEmail() {
 }
 
 document.querySelector('.order-button').addEventListener('click', redirectToEmail);
-var expandBtn = document.querySelector('.expand-btn');
-var expandText = document.querySelector('.expand-text');
-
-expandBtn.addEventListener('click', function() {
-  if (expandText.style.display === 'none') {
-    expandText.style.display = 'inline';
-    expandBtn.textContent = 'Weniger anzeigen';
-  } else {
-    expandText.style.display = 'none';
-    expandBtn.textContent = 'Mehr anzeigen';
-  }
-});
-
